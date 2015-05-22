0e8ebc7c865909587cfaa0364ba022872b289a4c
Timur Kunayev <timur.kunayev@gmail.com>
2015-05-21 19:40:00 -0600

    updated login

--- client/assets/javascripts/components/Login.jsx
+++ client/assets/javascripts/components/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Modal from 'react-bootstrap/lib/Modal';
-import Button from 'react-bootstrap/lib/Button';
 import Input from 'react-bootstrap/lib/Input';
 import Alert from 'react-bootstrap/lib/Alert';
 import AuthStore from '../stores/AuthStore';
@@ -9,25 +8,28 @@ import AuthActions from '../actions/AuthActions';
 const Login = React.createClass({
   displayName: 'Login',
 
-  onLogin(){
+  onLogin(e){
+      e.preventDefault();
       this.props.onLoginSubmit(this.refs.email.getValue(), this.refs.password.getValue());
   },
 
   render() {
 
     var error = this.props.error ? <Alert bsStyle="danger">{this.props.error}</Alert> : ""
+    var fl = {float: "left" };
     return (
         <Modal {...this.props} title='Login' animation={true}>
-            <div className='modal-body'>
-              <form className='form-horizontal'>
-                 {error}
-                <Input type='text' label='Email' labelClassName='col-xs-2' wrapperClassName='col-xs-10' ref="email" />
-                <Input type='password' label='Password' labelClassName='col-xs-2' wrapperClassName='col-xs-10' ref="password" />
-              </form>
-            </div>
-            <div className='modal-footer'>
-                <Button onClick={this.onLogin}>Login</Button>
-            </div>
+            <form className='form-horizontal'>
+                <div className='modal-body'>
+                    {error}
+                    <Input type='email' label='Email' labelClassName='col-xs-2' wrapperClassName='col-xs-10' ref="email" />
+                    <Input type='password' label='Password' labelClassName='col-xs-2' wrapperClassName='col-xs-10' ref="password" />
+                </div>
+                <div className='modal-footer'>
+                    <Input type="submit" label="Login" onClick={this.onLogin} />
+                    <div > <a href="#">Sign Up For An Account</a></div>
+                </div>
+            </form>
         </Modal>
 
     );
